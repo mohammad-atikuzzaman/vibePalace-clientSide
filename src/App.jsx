4444d9d839cb5f7a08cdc130ab26fcaf06b468bf
mr-codex <mcodex47@gmail.com>
2024-05-10 10:58:36 +0600

    react icons installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
+import { FaBeer } from "react-icons/fa";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline text-red-500">
+        Hello world!
+      </h1>
+      <FaBeer />
       <button className="btn btn-primary">button</button>
     </>
   );
