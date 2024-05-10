be55e818ad2bef91220f23c110cf269686a78634
mr-codex <mcodex47@gmail.com>
2024-05-10 10:56:35 +0600

    daisy ui installed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-500">Hello world!</h1>
+      <button className="btn btn-primary">button</button>
     </>
   );
 }
